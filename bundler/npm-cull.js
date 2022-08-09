0ee6bcc2c5dac36bf4505635298a4c617a1191ce
arunvjn <arun@appsmith.com>
2022-08-09 14:01:56 +0530

    Fixes null pointer

--- bundler/npm-cull.js
+++ bundler/npm-cull.js
@@ -1,23 +1,23 @@
 var ChangesStream = require('changes-stream'),
-    normalizeRegistryMetadata = require('normalize-registry-metadata'),
-    log = require('minilog')('cull'),
-    semver = require('semver');
+  normalizeRegistryMetadata = require('normalize-registry-metadata'),
+  log = require('minilog')('cull'),
+  semver = require('semver');
 
 module.exports = function _cull(cache) {
   var aliases = cache.db,
-      pubStream = new ChangesStream({
-        db: 'https://replicate.npmjs.com',
-        include_docs: true,
-        since: 'now'
-      });
+    pubStream = new ChangesStream({
+      db: 'https://replicate.npmjs.com',
+      include_docs: true,
+      since: 'now'
+    });
 
   pubStream.on('data', function (data) {
     var module = data.id,
-        doc = normalizeRegistryMetadata(data.doc),
-        version = doc['dist-tags'].latest, // Breaks under force publishes
-        start = module,                    // of olde versions...
-        end = start.slice(0, start.length - 1)     // TODO: Ask rvagg about this
-              + String.fromCharCode(start.charCodeAt(start.length - 1) + 1);
+      doc = normalizeRegistryMetadata(data.doc),
+      version = doc['dist-tags'] ? doc['dist-tags'].latest : 1, // Breaks under force publishes
+      start = module,                    // of olde versions...
+      end = start.slice(0, start.length - 1)     // TODO: Ask rvagg about this
+        + String.fromCharCode(start.charCodeAt(start.length - 1) + 1);
 
     var stream = aliases.createKeyStream({ start: start, end: end });
 
